a13b447c2fb49c105d6db48a905772d0a1bc23c5
MathisD49 <mathisdubuc@gmail.com>
2020-11-10 10:22:15 +0100

    amelioration du code du module processor (7.5/10 pylint)

--- classes/Processor.py
+++ classes/Processor.py
@@ -1,4 +1,13 @@
+"""
+module for creating a processor object
+"""
+
 class Processor:
+
+    """
+    class for creating a processor object
+    """
+
     def __init__(self, number, brand):
         self.number = number
         self.brand = brand
