34c9f491e91bcd922d1af12ad3f5a8e5b447c018
MathisD49 <mathisdubuc@gmail.com>
2020-11-10 10:24:04 +0100

    amelioration du code du module ram (7./10 pylint)

--- classes/ram.py
+++ classes/ram.py
@@ -1,4 +1,13 @@
+"""
+module for creating a processor object
+"""
+
 class RAM:
+
+    """
+    class for creating a ram object
+    """
+
     def __init__(self, number, brand):
         self.number = number
         self.brand = brand
