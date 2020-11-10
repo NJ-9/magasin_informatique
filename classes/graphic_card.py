0f4e1089a0854dcdb3e68db9da1cf56657810601
MathisD49 <mathisdubuc@gmail.com>
2020-11-10 10:17:52 +0100

    amélioration du code du module graphic_card (7.5/10 pylint)

--- classes/graphic_card.py
+++ classes/graphic_card.py
@@ -1,4 +1,13 @@
+"""
+module for creating a graphic card object
+"""
+
 class GraphicCard:
+
+    """
+    class for creating a graphic card object
+    """
+
     def __init__(self, number, brand):
         self.number = number
         self.brand = brand
