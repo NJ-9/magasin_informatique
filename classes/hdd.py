6291eb610c23f6d57a1ed8c9f01cb962033e6964
MathisD49 <mathisdubuc@gmail.com>
2020-11-10 10:20:20 +0100

    amelioration du code du module hdd (7.5/10 pylint)

--- classes/hdd.py
+++ classes/hdd.py
@@ -1,4 +1,13 @@
+"""
+module for creating a hdd object
+"""
+
 class HDD:
+
+    """
+    class for creating a hdd object
+    """
+
     def __init__(self, number, brand):
         self.number = number
         self.brand = brand
